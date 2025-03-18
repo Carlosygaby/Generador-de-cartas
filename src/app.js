1b2c6953e7505d67cddb1145bbc7e22c24f7b970
Carlosygaby <191144185+Carlosygaby@users.noreply.github.com>
2025-03-18 12:36:53 +0100

    Actualizando app.js index.html stytle.css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,60 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generarCarta = () => {
+  const numeroAlAzar = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  let figuras = ["♠", "♥", "♦", "♣"];
+  let numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  let colores = ["red", "green", "black", "blue"];
+
+  let carta = document.querySelector(".card");
+
+  let figura = document.querySelector(".figura");
+  figura.innerHTML = numeroAlAzar(figuras);
+  figura.style.color = numeroAlAzar(colores);
+
+  if (figura.style.color === "black") {
+    carta.style.backgroundColor = "rgba(129, 122, 121, 0.8)";
+  }
+  if (figura.style.color === "red") {
+    carta.style.backgroundColor = "rgba(223, 206, 206, 0.84)";
+  }
+  if (figura.style.color === "green") {
+    carta.style.backgroundColor = "rgba(189, 219, 189, 0.8)";
+  }
+  if (figura.style.color === "blue") {
+    carta.style.backgroundColor = "rgba(180, 180, 218, 0.81)";
+  }
+
+  let figuraAbajo = document.querySelector(".abajo");
+  figuraAbajo.innerHTML = figura.innerHTML;
+  figuraAbajo.style.color = figura.style.color;
+
+  let listaNumeros = document.getElementsByClassName("numeros");
+  Array.from(listaNumeros).forEach((numero) => {
+    numero.innerText = numeroAlAzar(numeros);
+    numero.style.color = figura.style.color;
+  });
+};
+window.generarCarta = generarCarta;
+window.onload = function () {
+  generarCarta();
 };
